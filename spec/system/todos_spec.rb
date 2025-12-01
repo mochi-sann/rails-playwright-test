5dc78ff1371b35bcebfd795e8aba9b0f31a5357d
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-12-01 23:01:39 +0900

    system test

--- spec/system/todos_spec.rb
+++ spec/system/todos_spec.rb
@@ -1,7 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "Todos", type: :system do
-  it "shows validation errors when title is blank", skip: "Capybara server socket creation is blocked in this environment" do
+  it "creates a todo successfully" do
+    visit root_path
+
+    click_on "New todo"
+    fill_in "Title", with: "Write system spec"
+    fill_in "Description", with: "Use rack_test driver"
+    click_on "Create Todo"
+
+    expect(page).to have_text("Todo was successfully created")
+    expect(page).to have_text("Write system spec")
+  end
+
+  it "shows validation errors when title is blank" do
     visit new_todo_path
 
     fill_in "Description", with: "Some description"
